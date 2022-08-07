1ae841f9645bef19076817784fc6b11da3854f03
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2022-08-07 19:40:05 +0530

    added error and download ui

--- public/script.js
+++ public/script.js
@@ -7,20 +7,23 @@ form.addEventListener("submit", (e) => {
     .post("/api/upload", formData)
     .then((res) => {
       if (res.data.filename) {
-        console.log(res.data.filename);
+        //console.log(res.data.filename);
         showButton(res.data.filename);
       } else {
         throw new Error("Error Generating Calendar file");
       }
     })
     .catch((err) => {
-      console.log(err);
+      //console.log(err);
+      showError(err);
     });
 });
 
 function showButton(filename) {
   const downloadButton = document.querySelector(".button");
-  downloadButton.classList.remove("hidden");
+  const buttondiv = document.querySelector("#buttondiv");
+  buttondiv.classList.remove("hidden");
+  buttondiv.classList.add("flex");
   downloadButton.href = `/download/${filename}`;
 }
 
@@ -28,6 +31,14 @@ const uploadbox = document.querySelector("#file-upload");
 uploadbox.onchange = () => {
   if (uploadbox.files.length > 0) {
     const fileName = document.querySelector("#filename");
-    fileName.textContent = "Uploaded File : " + uploadbox.files[0].name;
+    fileName.textContent = "Uploaded file: " + uploadbox.files[0].name;
   }
 };
+
+function showError(error) {
+  const errordiv = document.querySelector("#error");
+  const errortext = document.querySelector("#errortext");
+  errortext.innerHTML = error.response.data.message || error.message;
+  errordiv.classList.remove("hidden");
+  errordiv.classList.add("flex");
+}
